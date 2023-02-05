6c24816818c74a59faf1bac3e70d9b5ddc1eed81
Steve Krenn <stevekrenn@gmail.com>
2023-02-05 17:36:35 -0600

    installed apollo server, graphQL in root project.json / added apollo scripts in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 function App() {
   return (
