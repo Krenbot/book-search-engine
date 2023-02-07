9fe9852497a62481701df729a3a99734e8659f88
Steve Krenn <stevekrenn@gmail.com>
2023-02-07 16:10:54 -0600

    added type defs based on client specified params

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int 
+        savedBooks: [Book]
+    }
+    
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    
+    type Query {
+        me: User
+    }
+   
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: String!): User
+    }
+`
+
+module.exports = typeDefs
