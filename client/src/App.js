43bb340d103d66b18a08eaf49f894a3719a123ed
Steve Krenn <stevekrenn@gmail.com>
2023-02-09 17:59:06 -0600

    added jwt request-to-backend logic in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
@@ -13,7 +24,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
+      <Router>
         <>
           <Navbar />
           <Switch>
@@ -22,7 +33,7 @@ function App() {
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
         </>
-      </BrowserRouter>
+      </Router>
     </ApolloProvider>
   );
 }
