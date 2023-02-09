53f043aadbc8525f3ba53433c2b0b714456a59be
Steve Krenn <stevekrenn@gmail.com>
2023-02-09 17:43:28 -0600

    added error handling to adding user

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -13,11 +13,22 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser] = useMutation(ADD_USER)
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+
+    setUserFormData(
+      { ...userFormData, [name]: value }
+    );
   };
 
   const handleFormSubmit = async (event) => {
