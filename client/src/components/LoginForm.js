6cad51dcdc036d1ba925d9a049fa5c1c465a3f6d
Steve Krenn <stevekrenn@gmail.com>
2023-02-08 18:00:15 -0600

    added loginform.js to use the login_user mutation instead of api

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -15,6 +17,8 @@ const LoginForm = () => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  const [login] = useMutation(LOGIN_USER)
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -25,16 +29,20 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
+    // try {
+    //   const response = await loginUser(userFormData);
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await login({ variables: { ...userFormData } })
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
